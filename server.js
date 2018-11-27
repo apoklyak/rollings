6ab00deb986c29c4233a24390fd034a3f10a67a1
Andrey Poklyak <andrey.poklyak@gmail.com>
2018-11-27 15:45:32 +0300

    Add server to .gz compression

---
+++ server.js
@@ -0,0 +1,15 @@
+var compression = require('compression');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(compression());
+app.use(express.static(__dirname + '/dist/rollings'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/rollings/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+
+console.log('Server is listening');
