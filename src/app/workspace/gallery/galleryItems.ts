49f5bdb6ee6f223ae6416c173d3f7e2770d32df7
Natallia Nikitina <nikitinanf@gmail.com>
2020-01-29 13:48:53 +0300

    change path

--- src/app/workspace/gallery/galleryItems.ts
+++
@@ -1,8 +0,0 @@
-export const galleryItems = [
-  {path:  '../../../assets/img/products/17.jpg'},
-  {path:  '../../../assets/img/products/18.jpg'},
-  {path:  '../../../assets/img/products/19.jpg'},
-  {path:  '../../../assets/img/products/21.jpg'},
-  {path:  '../../../assets/img/products/16.jpg'},
-]
-
